4308575cc71d1c14a75772dcbf87071e75753193
Subhrojit Hore <subhrojithore35@gmail.com>
2026-02-08 00:16:03 +0530

    chore: separation of backend and frontend branches

--- Backend/Server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./App");
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
